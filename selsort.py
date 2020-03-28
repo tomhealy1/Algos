4f862176c9156f6018fb81f8ba5913d1894977e5
tomhealy1 <alfaone156@gmail.com>
2020-03-28 13:06:22 +0000

    added selection sort python code + typo

--- selsort.py
+++ selsort.py
@@ -6,14 +6,14 @@ stop = timeit.default_timer()
 execution_time = stop - start
 
 def selSort(nlist):
-    for fillslot in range(len(nlist)-1,0,-1):
+    for slot in range(len(nlist)-1,0,-1):
         maxpos=0
-        for location in range(1, fillslot+1):
+        for location in range(1, slot+1):
             if nlist[location]>nlist[maxpos]:
                 maxpos= location
 
-        temp = nlist[fillslot]
-        nlist[fillslot] = nlist[maxpos]
+        temp = nlist[slot]
+        nlist[slot] = nlist[maxpos]
         nlist[maxpos] = temp
 
 nlist = [2,45,3,22,56,88,1212,34,9696,3,5,34,5,3,65,34,54,1,0]
